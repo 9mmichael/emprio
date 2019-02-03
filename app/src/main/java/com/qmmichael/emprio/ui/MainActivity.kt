e7e6f4f1f3fba0564ec45d7cb3b8b5861b1a255b
9mmichael <ueno.michael@gmail.com>
2019-02-03 18:45:02 +0900

    Initial commit

---
+++ app/src/main/java/com/qmmichael/emprio/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.qmmichael.emprio.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.qmmichael.emprio.R
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+//    setContentView(layout.main_activity)
+    setContentView(R.layout.main_activity)
+
+  }
+}
